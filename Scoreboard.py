2e1378a2fb46755fd60142815e24136d89c1e8ae
lokesh0177 <lokesharya78@gmail.com>
2022-10-15 16:51:29 +0530

    Initial Commit

--- Scoreboard.py
+++ Scoreboard.py
@@ -7,8 +7,12 @@ FONT = ('Courier', 25, 'normal')
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+
+
         self.goto(0, 260)
         self.penup()
         self.color("white")
@@ -22,6 +26,9 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+
 
         self.score = 0
         self.update_scoreboard()
